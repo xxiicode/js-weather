72d20ca9d06c0da94829b4c0e9ff5ab4cccd5838
xxiicode <leandrobilokapic@gmail.com>
2024-08-08 19:25:37 +0200

    MVP

---
+++ script.js
@@ -0,0 +1,90 @@
+let local_api_key = '6be4ce7e804dc9e79cdd57582019131a'
+let lang = 'en' // english by default
+const searchbutton = document.getElementById('searchButton')
+const divContent = document.getElementById('weatherData')
+const cityInput = document.getElementById('cityName');
+const buttonEng = document.getElementById('english')
+const buttonSpa = document.getElementById('espanol')
+const URL_base = 'https://api.openweathermap.org/data/2.5/weather'
+const translation = {
+    en: {
+        title: 'Another Weather App ⛅',
+        placeholder: 'Please, enter a city name',
+        searchButton: 'Search',
+        noCity: 'Unknown location',
+        hum: 'Humidity'
+    },
+    es: {
+        title: 'Otra Aplicación del Clima ⛅',
+        placeholder: 'Por favor, ingrese el nombre de una ciudad',
+        searchButton: 'Buscar',
+        noCity: 'Ciudad no encontrada',
+        hum: 'Humedad'
+    }
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const city =cityInput.value
+    if(city) {
+        fetchWeather(city)
+    }
+}
+
+function fetchWeather(city){
+    fetch(`${URL_base}?q=${city}&appid=${local_api_key}&&units=metric&lang=${lang}`)
+    .then(data => data.json())
+    .then(data => showData(data))
+}
+
+function showData(data) {
+    divContent.innerHTML = ''
+    console.log(data)
+    const cityName = data.name || translation[lang].noCity
+    const cityCountry = data.sys ? data.sys.country : ''
+    const temperature = data.main ? data.main.temp : 'N/A';
+    const humidity = data.main? data.main.humidity: 'N/A'
+    const description = data.weather && data.weather.length > 0 ? data.weather[0].description : '';
+
+    const cityTitle = document.createElement('h2')
+    cityTitle.textContent = cityName !== translation[lang].noCity ?  `${cityName}, ${cityCountry}` : translation[lang].noCity
+    const cityTemperature = document.createElement('p')
+    cityTemperature.textContent = temperature !== 'N/A' ? `${temperature} °C` : temperature;
+    const cityHumidity = document.createElement('p')
+    cityHumidity.textContent = humidity !== 'N/A' ? ` ${translation[lang].hum} :  ${humidity} %` : humidity;
+
+
+    const descriptionInfo = document.createElement('p')
+    descriptionInfo.textContent = description
+
+    divContent.appendChild(cityTitle)
+    divContent.appendChild(cityTemperature)
+    divContent.appendChild(cityHumidity)
+    divContent.appendChild(descriptionInfo)
+}
+
+const changeLang = (lang) => {
+    document.getElementById('title').innerText = translation[lang].title
+    document.getElementById('heading').innerText = translation[lang].title
+    document.getElementById('cityName').placeholder = translation[lang].placeholder
+    document.getElementById('searchButton').innerText = translation[lang].searchButton
+}
+
+buttonEng.addEventListener('click', () => {
+    lang = 'en';
+    changeLang(lang)
+    if(cityInput.value !== '') {
+        fetchWeather(cityInput.value)
+    } else {divContent.innerHTML = ''}
+    buttonEng.classList.add('active');
+    buttonSpa.classList.remove('active');
+});
+buttonSpa.addEventListener('click', () => {
+    lang = 'es';
+    changeLang(lang)
+    if(cityInput.value !== '') {
+        fetchWeather(cityInput.value)
+    } else {divContent.innerHTML = ''}
+    buttonSpa.classList.add('active');
+    buttonEng.classList.remove('active');
+});
